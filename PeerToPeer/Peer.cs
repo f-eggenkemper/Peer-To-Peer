8ec8560f046ab38b2b6aec012605d57220368930
Florian Schniederjann <florian.schniederjann@gmx.net>
2017-10-17 13:23:28 +0200

    Initial Upload

---
+++ PeerToPeer/Peer.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeerToPeer
+{
+    class Peer : IComparable
+    {
+        public const int TTL = 1;
+        public int data;
+        string Name;
+        bool alive;
+        SortedSet<Peer> Peers = new SortedSet<Peer>();
+
+        public Peer(string name, int data)
+        {
+            this.alive = true;
+            this.Name = name;
+            this.data = data;
+        }
+
+        public SortedSet<Peer> Ping(Peer p, int ttl)
+        {
+            SortedSet<Peer> ReturnPeers = new SortedSet<Peer>();
+            if (p.Equals(this) || !this.alive)
+            {
+                return ReturnPeers;
+            }
+            ReturnPeers.Add(this);
+
+            if (ttl > 0)
+            {
+                SortedSet<Peer> PeersCopy = new SortedSet<Peer>();
+                PeersCopy.UnionWith(Peers);
+                foreach (Peer peer in PeersCopy)
+                {
+                    ReturnPeers.UnionWith(peer.Ping(p, ttl - 1));
+                }
+
+                Peers.Add(p);
+            }
+
+            return ReturnPeers;
+        }
+
+        public void Netzeintritt()
+        {
+            SortedSet<Peer> peers2 = new SortedSet<Peer>();
+            foreach (Peer peer in Peers)
+            {
+                peers2.UnionWith(peer.Ping(this, TTL));
+            }
+            Peers = peers2;
+        }
+
+        public void Add(Peer p)
+        {
+            Peers.Add(p);
+            p.Peers.Add(this);
+        }
+
+        public Peer search(int suchwort, int ttl)
+        {
+            if (this.data == suchwort)
+            {
+                return this;
+            }
+
+            if (ttl > 0)
+            {
+                foreach (Peer p in Peers)
+                {
+                    Peer result = p.search(suchwort, ttl - 1);
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                }
+            } 
+
+            return null;
+        }
+
+        public void PrintList()
+        {
+            Console.WriteLine("Ich bin " + this.Name);
+            foreach (Peer p in Peers)
+            {
+                Console.Write(p.Name + ", ");
+            }
+            Console.WriteLine(" ");
+        }
+
+        public int CompareTo(object obj)
+        {
+            return Name.CompareTo(((Peer)obj).Name);
+        }
+
+        public String ToString()
+        {
+            return this.Name;
+        }
+    }
+}
---
+++ PeerToPeer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeerToPeer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Peer p1 = new Peer("Peer 1", 110);
+            Peer p2 = new Peer("Peer 2", 22);
+            Peer p3 = new Peer("Peer 3", 33);
+            Peer p4 = new Peer("Peer 4", 33);
+            Peer p5 = new Peer("Peer 5", 33);
+            Peer p6 = new Peer("Peer 6", 666);
+
+            p1.Add(p2);
+            p2.Add(p3);
+            p1.Add(p4);
+            p4.Add(p5);
+            p4.Add(p6);
+            p5.Add(p6);
+
+            Peer p7 = new Peer("Peer 7", 77);
+            p7.Add(p2);
+            p7.Add(p3);
+            p7.Netzeintritt();
+
+            p1.PrintList();
+            p7.PrintList();
+
+            Console.WriteLine(p7.search(110, Peer.TTL).ToString());
+            Console.WriteLine(p7.search(666, Peer.TTL).ToString());
+        }
+    }
+}
